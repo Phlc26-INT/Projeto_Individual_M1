9078d1c15a3cf6e333dd5373227c98c5367e04ba
Pedro Castro <pedro.castro@sou.inteli.edu.br>
2025-03-21 11:34:44 -0300

    Adicionando Versão Mobile

--- src/scenes/MenuSceneMobile.js
+++ src/scenes/MenuSceneMobile.js
@@ -1,6 +1,6 @@
-class MenuScene extends Phaser.Scene {
+class MenuSceneMobile extends Phaser.Scene {
     constructor() {
-        super({ key: 'MenuSceneDesktop' }); //Aqui vamos usar o atributo Key da classe pai
+        super({ key: 'MenuSceneMobile' }); //Aqui vamos usar o atributo Key da classe pai
     }
     preload() {
         // Aqui vamos carregar o background do menu
@@ -10,20 +10,19 @@ class MenuScene extends Phaser.Scene {
 
     create() {
         // Adicionando o background do menu
-        this.add.tileSprite(0, 0, this.scale.width, this.scale.height, 'Nebula1').setOrigin(0, 0);
+        this.add.tileSprite(0, 0, larguraJogo, alturaJogo, 'Nebula1').setOrigin(0, 0);
         
         // Aqui estamos colocando o título do jogo
-        const title = this.add.text(this.scale.width / 2, this.scale.height / 3, 'ASTEROID ESCAPE',{
-                fontSize: '55px',
-                fontFamily: 'Arial',
-                color: '#ffffff',
-                shadow: { blur: 10, color: '#0066ff', fill: true }
-            }
-        ).setOrigin(0.5);
+        const title = this.add.text(this.scale.width / 2, this.scale.height / 3, 'ASTEROID ESCAPE', {
+            fontSize: '32px', // Aumentei o tamanho da fonte para melhor visibilidade
+            fontFamily: 'Arial',
+            color: '#ffffff',
+            shadow: { blur: 10, color: '#0066ff', fill: true }
+        }).setOrigin(0.5);
 
         // Aqui vamos criar o botão de início
         const startButton = this.add.text(this.scale.width / 2,this.scale.height / 2 + 90,'Iniciar Jogo',{
-                fontSize: '32px',
+                fontSize: '20px',
                 fontFamily: 'Arial',
                 color: '#ffffff',
                 padding: { x: 20, y: 10 },
@@ -33,20 +32,20 @@ class MenuScene extends Phaser.Scene {
         .on('pointerdown', () => this.startGame()); //Essa  função detecta o click do mouse no botão
 
        //Aqui vamos crirar o texto de tutorial
-        this.add.text(this.scale.width / 2 ,this.scale.height / 2 + 150,'Como jogar: Controle a nave com as setas do teclado e escape dos asteroides o quanto você conseguir!',{
-            fontSize: '25px',
+        this.add.text(this.scale.width / 2 ,this.scale.height / 2 + 150,'Como jogar: Controle a nave com o touch do celular e escape dos asteroides o quanto você conseguir!',{
+            fontSize: '17px',
             fontFamily: 'Arial',
             color: '#ffffff',
             align: 'center',
             padding: { x: 20, y: 10 },
-        }).setOrigin(0.5)
+        }).setOrigin(0.5)   
 
     }
     startGame() {
         // Aqui fazemos a transição para a cena do jogo com um fadeout
         this.cameras.main.fade(500, 0, 0, 0);
         this.cameras.main.once('camerafadeoutcomplete', () => {
-        this.scene.start('GameScene', this.game);
+        this.scene.start('GameSceneMobile', this.game);
         });
     }
 
